209e1fb89310038c9eabf8046f0fa7c87fc0ab42
iyunbo <yunbo.wang@solven.eu>
2019-04-19 18:44:37 +0200

    minor fix

--- 1-quick_scoring.py
+++ 1-quick_scoring.py
@@ -1,6 +1,8 @@
 import datetime
 import time
 
+import numpy as np
+
 from trainer.model import quick_score
 from trainer.preparation import load_data, extract_features
 
@@ -8,10 +10,11 @@ from trainer.preparation import load_data, extract_features
 def main():
     df, df_store = load_data(debug=False)
     feat_matrix, features_x, feature_y = extract_features(df, df_store)
-    score, duration = quick_score(feat_matrix.loc[~(feat_matrix['Type'] == 'test')], features_x, feature_y)
+    score1, score2, score3, duration = quick_score(feat_matrix.loc[~(feat_matrix['Type'] == 'test')], features_x,
+                                                   feature_y)
     ts = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
     print(ts)
-    print('RMSPE = %.4f' % score)
+    print('RMSPE = %.4f' % np.mean([score1, score2, score3]))
     print('Duration : %.2f seconds' % duration)
 
 
